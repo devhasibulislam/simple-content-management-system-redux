01ce2698986d6b9042efe1baf68016969cd6c559
hasibulislam999 <hasib143sl@gmail.com>
2022-12-09 21:02:14 +0600

    add blog page init

---
+++ client/src/pages/dashboard/AddBlog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddBlog = () => {
+  return (
+    <section className="flex flex-col justify-center items-center h-full w-full ">
+      This is Add blog page
+    </section>
+  );
+};
+
+export default AddBlog;

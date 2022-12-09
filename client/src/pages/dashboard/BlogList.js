211751477c74f0d64763e9d734502ca30e760455
hasibulislam999 <hasib143sl@gmail.com>
2022-12-09 21:02:21 +0600

    blog list page init

---
+++ client/src/pages/dashboard/BlogList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BlogList = () => {
+  return (
+    <section className="flex flex-col justify-center items-center h-full w-full ">
+      This is Blog list page
+    </section>
+  );
+};
+
+export default BlogList;

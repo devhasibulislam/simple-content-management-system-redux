107c286dc9b615aaba54a8a3428dcc1ab5e6a08c
hasibulislam999 <hasib143sl@gmail.com>
2022-12-10 15:30:32 +0600

    integrate redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={routes} />
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
     </div>
   );
 }

b7fb192b3af47c380da91cf9717976c2a778809c
hasibulislam999 <hasib143sl@gmail.com>
2022-12-10 15:28:58 +0600

    integrate redux criteria

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import BlogCard from "../components/BlogCard";
-import useBlogs from "../hooks/useBlogs";
+import getBlogs from "../redux/thunks/getBlogs";
 
 const Home = () => {
-  const blogs = useBlogs();
-  const activeClass = "bg-purple-500 border-0 text-white"
+  const dispatch = useDispatch();
+  const { blogs } = useSelector((state) => state.blog);
+
+  useEffect(() => {
+    dispatch(getBlogs());
+  }, [dispatch]);
+
+  const activeClass = "bg-purple-500 border-0 text-white";
 
   return (
     <section className="container mx-auto">
       <div className="flex flex-col gap-y-4 mt-4">
         <div className="flex gap-x-2 lg:px-0 md:px-0 px-4">
-          <button className={`border rounded-3xl py-2 px-4 font-semibold text-sm ${activeClass}`}>
+          <button
+            className={`border rounded-3xl py-2 px-4 font-semibold text-sm ${activeClass}`}
+          >
             First upload
           </button>
-          <button className={`border rounded-3xl py-2 px-4 font-semibold text-sm`}>
+          <button
+            className={`border rounded-3xl py-2 px-4 font-semibold text-sm`}
+          >
             Last upload
           </button>
         </div>

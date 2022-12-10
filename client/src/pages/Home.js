9bbb3361db7deca0713da4686810b03107568659
hasibulislam999 <hasib143sl@gmail.com>
2022-12-10 18:41:45 +0600

    home page modify

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import BlogCard from "../components/BlogCard";
+import { toggleSortBy } from "../redux/actions/blogActions";
 import getBlogs from "../redux/thunks/getBlogs";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { blogs } = useSelector((state) => state.blog);
+  const { blogs, sortBy } = useSelector((state) => state.blog);
 
   useEffect(() => {
     dispatch(getBlogs());
@@ -18,20 +19,20 @@ const Home = () => {
       <div className="flex flex-col gap-y-4 mt-4">
         <div className="flex gap-x-2 lg:px-0 md:px-0 px-4">
           <button
-            className={`border rounded-3xl py-2 px-4 font-semibold text-sm ${activeClass}`}
+            className={`border rounded-3xl py-2 px-4 font-semibold text-sm ${
+              sortBy ? activeClass : null
+            }`}
+            onClick={() => dispatch(toggleSortBy())}
           >
-            First upload
-          </button>
-          <button
-            className={`border rounded-3xl py-2 px-4 font-semibold text-sm`}
-          >
-            Last upload
+            {!sortBy ? "First upload" : "Last upload"}
           </button>
         </div>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 lg:px-0 md:px-0 px-4">
-          {blogs.map((blog) => (
-            <BlogCard key={blog._id} blog={blog} />
-          ))}
+          {!sortBy
+            ? blogs.map((blog) => <BlogCard key={blog._id} blog={blog} />)
+            : blogs
+                .map((blog) => <BlogCard key={blog._id} blog={blog} />)
+                .reverse()}
         </div>
       </div>
     </section>

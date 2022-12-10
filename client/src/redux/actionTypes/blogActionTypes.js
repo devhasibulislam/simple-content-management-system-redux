94a53b424c666c26d251064c92f61731dbb5480c
hasibulislam999 <hasib143sl@gmail.com>
2022-12-10 15:29:36 +0600

    blog action type init

---
+++ client/src/redux/actionTypes/blogActionTypes.js
@@ -0,0 +1,8 @@
+const blogActionTypes = {
+  ADD_BLOG: "ADD_BLOG",
+  REMOVE_BLOG: "REMOVE_BLOG",
+  FETCH_BLOGS: "FETCH_BLOGS",
+  UPDATE_BLOG: "UPDATE_BLOG",
+};
+
+export default blogActionTypes;

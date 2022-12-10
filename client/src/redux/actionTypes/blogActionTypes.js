a9dedd342cccdd83b4eaf5ce50d3dcc240bcde39
hasibulislam999 <hasib143sl@gmail.com>
2022-12-10 18:42:02 +0600

    blog action types modify

--- client/src/redux/actionTypes/blogActionTypes.js
+++ client/src/redux/actionTypes/blogActionTypes.js
@@ -4,6 +4,7 @@ const blogActionTypes = {
   GET_CONTENT: "GET_CONTENT",
   FETCH_BLOG: "FETCH_BLOG",
   UPDATE_CONTENT: "UPDATE_CONTENT",
+  TOGGLE_SORTBY: "TOGGLE_SORTBY",
 };
 
 export default blogActionTypes;
